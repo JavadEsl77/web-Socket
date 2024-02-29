dd27b341dacfe0db8941929f860777fb70ee2f43
javadEsl <javades755@gmail.com>
2024-02-29 23:27:34 +0330

    start basic config | server and client

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import io from 'socket.io-client'
+import {useEffect, useState} from "react";
+
+const socket = io('http://localhost:3001')
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [userMessage, setUserMessage] = useState('')
+    const [messageList, setMessageList] = useState([])
+
+    useEffect(() => {
+        socket.on("receive_message", (serverMessage) => {
+            setMessageList((prevMessageList) => [...prevMessageList, serverMessage]);
+        });
+    }, [socket]);
+
+
+    const handlerSendMessage = () => {
+        // const text = document.querySelector('input').value
+        socket.emit('send-message', {message: userMessage})
+    }
+
+    const handlerWriteMessage = (event) => {
+        setUserMessage(event.target.value)
+    }
+
+    return (
+        <div className="App">
+            <p>welcome to webSocket Application :)</p>
+            <input placeholder={'message'} value={userMessage} onChange={handlerWriteMessage}/>
+            <button onClick={handlerSendMessage}>Send Message</button>
+            <ul>
+                {Array.isArray(messageList) && messageList.map((message, index) => (
+                    <li key={index}>{message.message}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
